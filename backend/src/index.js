e10af59f23f5a1d23e97d26aa0c32e62103a3023
unknown <abdealidahodwala163@gmail.com>
2024-11-14 17:11:38 +0530

    Deploy tests

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,6 +14,9 @@ app.use(cors({origin:process.env.FRONTEND_URL}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/",(req,res)=>{
+    res.send("Hello World")
+})
 app.use("/user",userRouter)
 app.use("/movie",movieRouter)
 app.use("/tv",tvRouter)
