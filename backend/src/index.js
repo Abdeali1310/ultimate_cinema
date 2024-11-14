7e19ac1ee7813adc0a9ec57a0222957c3740c0e8
unknown <abdealidahodwala163@gmail.com>
2024-11-14 17:35:42 +0530

    Deploy tests

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,15 +9,24 @@ const authRouter = require("./routers/authRouter");
 const reviewRouter = require("./routers/reviewRouter")
 
 require('dotenv').config();
+
+app.use(cors({
+    origin: "https://ultimate-cinema-client.vercel.app",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true
+}));
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "https://ultimate-cinema-client.vercel.app");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     next();
 });
 
-app.use(cors({ origin: "*" }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.send("Hello World")
